467f55a5ac2503239279d6f8c5b35b5b9e75b5b6
asummerville <alden.summerville@arup.com>
2025-03-07 15:29:45 -0500

    march output whitbox

--- jupyter/make_catchments.py
+++ jupyter/make_catchments.py
@@ -15,6 +15,13 @@ import warnings
 import argparse
 from tqdm import tqdm
 
+import osmnx as ox
+import geopandas as gpd
+import rasterio
+from rasterio.features import rasterize
+import numpy as np
+from shapely.geometry import box
+
 # filter warnings for now - code will need updating for Shapely 2.0+
 warnings.filterwarnings("ignore", category=ShapelyDeprecationWarning)
 warnings.filterwarnings("ignore", category=UserWarning) 
@@ -78,26 +85,28 @@ def make_profile(row,so,coords,profile_list,connection_list):
     row_order = row['Order']
     profile = profile_list[index] # grab full profile for segment
         
-    so_list = so.flat[profile].tolist() # get stream orders along full profile
-    so_list.reverse() # reversing to perform the next steps easier...
+    so_list = np.array(so.flat[profile]) # get stream orders along full profile
+    # so_list.reverse() # reversing to perform the next steps easier...
     
-    final_idx = len(so_list) - so_list.index(row_order) - 1
+    final_idx = len(so_list) - np.where(so_list[::-1] == row_order)[0][-1] - 1
+    # final_idx = len(so_list) - so_list.index(row_order) - 1
     # ^^^ get last instance of this stream order in the full profile. 
     
-    row['LocalPP'] = np.flip(coords[profile[final_idx]]) 
+    local_pp = np.flip(coords[profile[final_idx]]) 
     # ^^^ use idx to get the appropriate coords for the pour point.
     # np.flip() puts it in the right order
-    row['LocalPP_X'] = row['LocalPP'][0]
-    row['LocalPP_Y'] = row['LocalPP'][1]
+    # row['LocalPP_X'] = row['LocalPP'][0]
+    # row['LocalPP_Y'] = row['LocalPP'][1]
+    pour = pd.Series({'LocalPP_X': local_pp[0], 'LocalPP_Y': local_pp[1]})
     
+    # Commented out because we don't use the Profile or Chain or Final_SO attributes
+    # row['Profile'] = profile_list[index] # add full profile
+    # row['Chain'] = connection_list[index] # add full chain mapping    
     
-    row['Profile'] = profile_list[index] # add full profile
-    row['Chain'] = connection_list[index] # add full chain mapping    
-    
-    row['Final_SO'] = so.flat[profile][-1]
-    # idx of final segment along profile
-    row['Final_Chain_Val'] = connection_list[index][-1] 
-    return row
+    # row['Final_SO'] = so.flat[profile][-1]
+    # # idx of final segment along profile
+    # row['Final_Chain_Val'] = connection_list[index][-1] 
+    return pour
 
 def make_basin(row,grid,dem,fdir,
                routing,algorithm):
@@ -108,10 +117,14 @@ def make_basin(row,grid,dem,fdir,
     # row_order = row['Order']    
     x, y = row['LocalPP_X'], row['LocalPP_Y']
     c = grid.catchment(x=x,y=y,fdir=fdir,routing=routing,algorithm=algorithm)
+    # NOTE test if can remove this clipping
     grid.clip_to(c)
     # clipped_catch = grid.view(c)
+    # NOTE test with the below
+    # catchment_polygon = ops.unary_union(shape(shape) for shape, _ in grid.polygonize())
     catchment_polygon = ops.unary_union([geometry.shape(shape) 
                                      for shape, value in grid.polygonize()])
+    # NOTE test if this can be commented out if this clip isn't needed
     grid.viewfinder = dem.viewfinder
     
     row['BasinGeo']  = catchment_polygon
@@ -122,7 +135,7 @@ def make_basin(row,grid,dem,fdir,
 #                      Main Function
 ######################################################################
 
-def generate_catchments(grid, dem, condition_burn, dem_cd, acc_thresh=3000,so_filter_min=1, so_filter_max=4,
+def generate_catchments(grid, dem, dem_path, condition_burn, dem_cd, acc_thresh=3000,so_filter_min=1, so_filter_max=4,
                         routing='d8',algorithm='iterative', shoreline_clip=False):
     '''Full workflow integrating the above functions.
        Process is as follows: 
@@ -139,6 +152,7 @@ def generate_catchments(grid, dem, condition_burn, dem_cd, acc_thresh=3000,so_fi
     # dem = grid.read_raster(path)
     
     local_start = time.time()
+    step_start = time.time()
     
     if condition_burn == 0:
         print("Pysheds - conditioning DEM")
@@ -150,6 +164,10 @@ def generate_catchments(grid, dem, condition_burn, dem_cd, acc_thresh=3000,so_fi
         # Resolve flats in DEM
         inflated_dem = grid.resolve_flats(flooded_dem)
 
+        local_total = (time.time() - step_start)
+        print('Runtime ',str(local_total/60),'minutes')
+        step_start = time.time()
+
     # Specify directional mapping
     dirmap = (64, 128, 1, 2, 4, 8, 16, 32)
 
@@ -157,11 +175,15 @@ def generate_catchments(grid, dem, condition_burn, dem_cd, acc_thresh=3000,so_fi
     # Compute flow directions
     # -------------------------------------
     # fdir = grid.flowdir(dem, dirmap=dirmap)
+    if condition_burn == 2:
+        fdir = grid.flowdir(dem, dirmap=dirmap)
     if condition_burn == 0:
         fdir = grid.flowdir(inflated_dem, dirmap=dirmap)
     if condition_burn == 1:
         fdir = grid.flowdir(dem_cd, dirmap=dirmap)
-        
+    local_total = (time.time() - step_start)
+    print('Runtime ',str(local_total/60),'minutes')
+    step_start = time.time()
 
     # make flow accumulation raster
     print('Pysheds flow accumulaton map...')
@@ -172,6 +194,9 @@ def generate_catchments(grid, dem, condition_burn, dem_cd, acc_thresh=3000,so_fi
     so = grid.stream_order(fdir=fdir,mask=acc_mask)
     # update mask
     mask = acc_mask
+    local_total = (time.time() - step_start)
+    print('Runtime ',str(local_total/60),'minutes')
+    step_start = time.time()
     
     # make river network
     print('Pysheds stream network...')
@@ -184,14 +209,25 @@ def generate_catchments(grid, dem, condition_burn, dem_cd, acc_thresh=3000,so_fi
     print(branch_gdf.columns)
     branches_all = branch_gdf
 
+    local_total = (time.time() - step_start)
+    print('Runtime ',str(local_total/60),'minutes')
+    step_start = time.time()
+
     # generate profiles for each individual segment
     print('Pysheds generating profiles...')
     profiles, connections = grid.extract_profiles(fdir=fdir,mask=mask,include_endpoint=False)
+    local_total = (time.time() - step_start)
+    print('Runtime ',str(local_total/60),'minutes')
+    step_start = time.time()
     
     print('Pysheds calculating attributes...')
     branch_gdf = branch_gdf.apply(lambda x: calc_attributes(x,dem,profiles,so),axis=1)
     print(branch_gdf.columns)
     coords = dem.coords
+
+    local_total = (time.time() - step_start)
+    print('Runtime ',str(local_total/60),'minutes')
+    step_start = time.time()
 
     # Filter by stream order
     
@@ -202,16 +238,28 @@ def generate_catchments(grid, dem, condition_burn, dem_cd, acc_thresh=3000,so_fi
     if so_filter_max:
         branch_gdf = branch_gdf[(branch_gdf['Order']>=so_filter_min) & (branch_gdf['Order']<=so_filter_max)]
 
+    local_total = (time.time() - step_start)
+    print('Runtime ',str(local_total/60),'minutes')
+    step_start = time.time()
+
     # create all profile and connection lists w/ pour points
     print('Calculating pour points...')
-    branch_gdf = branch_gdf.apply(lambda x: make_profile(x,so,coords,profile_list,connection_list),axis=1)
+    # branch_gdf = branch_gdf.apply(lambda x: make_profile(x,so,coords,profile_list,connection_list),axis=1)
+    branch_gdf[['LocalPP_X', 'LocalPP_Y']] = branch_gdf.apply(lambda x: make_profile(x, so, coords, profile_list), axis=1)
+
+    # branch_drop_cols = ['Profile','Chain'] #,'Profile','Chain']
+    # branch_gdf.drop(branch_drop_cols,axis=1,inplace=True)
     
-    branch_gdf_copy = branch_gdf.copy() # unfiltered copy to return at end
+    # branch_gdf_copy = branch_gdf.copy() # unfiltered copy to return at end
     
     # some of these sections will have duplicate orders and pour points - drop them
     unique = branch_gdf[['Order','LocalPP_X','LocalPP_Y','Final_Chain_Val']].drop_duplicates()
     
     branch_gdf = branch_gdf.loc[unique.index]
+
+    local_total = (time.time() - step_start)
+    print('Runtime ',str(local_total/60),'minutes')
+    step_start = time.time()
     
 
     # Clip branches to IRL region****
@@ -222,6 +270,9 @@ def generate_catchments(grid, dem, condition_burn, dem_cd, acc_thresh=3000,so_fi
     # print(type(branches), branches.crs)
     branches = gpd.clip(branches, irl_region)
     print("Post IRL clip")
+    local_total = (time.time() - step_start)
+    print('Runtime ',str(local_total/60),'minutes')
+    step_start = time.time()
 
     if shoreline_clip == True:
         print("Pysheds branches - reading in shorelines")
@@ -243,48 +294,49 @@ def generate_catchments(grid, dem, condition_burn, dem_cd, acc_thresh=3000,so_fi
         shoreline_clip = shoreline_clip[shoreline_clip['Shape_Area'] > 0.000002] # Equivalent to approx > 5 acres
         print("shorelines filtered by attribute column")
         
+        local_total = (time.time() - step_start)
+        print('Runtime ',str(local_total/60),'minutes')
+        step_start = time.time()
 
         branch_gdf = gpd.clip(branch_gdf_i, shoreline_clip)
         print("Post shoreline clip")
         print(len(branch_gdf), "branches remain")
+
+        local_total = (time.time() - step_start)
+        print('Runtime ',str(local_total/60),'minutes')
+        step_start = time.time()
     else:
         branch_gdf = gpd.GeoDataFrame.from_features(branches,crs='epsg:4326')
     
-    
-
-    
-    
     print('Pysheds generating',len(branch_gdf),'catchments...')
-    branch_gdf = branch_gdf.apply(lambda x: make_basin(x,grid,dem,fdir,routing,algorithm),axis=1)
-
-    b_copy = branch_gdf.copy()
-    b_copy.set_geometry('BasinGeo',inplace=True)
-    b_copy.set_crs('epsg:4326',inplace=True)
+    basins_gdf = branch_gdf.apply(lambda x: make_basin(x,grid,dem,fdir,routing,algorithm),axis=1)
+    print("Pysheds - make basins finished")
     
-    copy_for_area = b_copy.copy()
+    local_total = (time.time() - step_start)
+    print('Runtime ',str(local_total/60),'minutes')
+    step_start = time.time()
+
+    # Set geometry and CRS directly on basins_gdf
+    basins_gdf.set_geometry('BasinGeo', inplace=True)
+    basins_gdf.set_crs('epsg:4326', inplace=True)
+    
+    copy_for_area = basins_gdf.copy()
     copy_for_area.to_crs('epsg:6933',inplace=True)
-    copy_for_area.geometry.area
     copy_for_area['AreaSqKm'] = copy_for_area.geometry.area  / 1000000
 
-    b_copy['AreaSqKm'] = copy_for_area['AreaSqKm']
+    basins_gdf['AreaSqKm'] = copy_for_area['AreaSqKm']
     
     basin_drop_cols = ['geometry','LocalPP'] #,'Profile','Chain']
-    branch_drop_cols = ['LocalPP'] #,'Profile','Chain']
+    # branch_drop_cols = ['LocalPP','Profile','Chain'] #,'Profile','Chain']
     
-    b_copy.drop(basin_drop_cols,axis=1,inplace=True)
-    branch_gdf_copy.drop(branch_drop_cols,axis=1,inplace=True)
-    
-    b_copy['Profile'] = b_copy['Profile'].astype(str)
-    b_copy['Chain'] = b_copy['Chain'].astype(str)
-
-    branch_gdf_copy['Profile'] = branch_gdf_copy['Profile'].astype(str)
-    branch_gdf_copy['Chain'] = branch_gdf_copy['Chain'].astype(str)
+    basins_gdf.drop(basin_drop_cols,axis=1,inplace=True)
+    # basins_gdf_copy.drop(branch_drop_cols,axis=1,inplace=True)
     
     local_total = (time.time() - local_start)
     
     print('Total runtime is',str(local_total/60),'minutes')
     
-    return b_copy, branch_gdf_copy, branches_all
+    return basins_gdf, branches_all
 
 
 
